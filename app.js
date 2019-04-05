ceb76bb07de9ec2dbb124b80f2fd28a481d68aba
latm33 <luistrejo033@gmail.com>
2019-04-04 20:15:10 -0700

    Cambios de nombres
    Cambio de nombre al controlador

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 angular.module('kanbanApp', [])
     .controller('kanbanListController', function () {
-        var todoList = this;
+        var kanbanCtrl = this;
         //Se accede a travez de la instancia porque tiene alcance local
-        todoList.kanbanList = [
+        kanbanCtrl.kanbanList = [
             //{ titulo: 'terminar todo list', done: false, paso: '1' },
             //{ titulo: 'deploy todo list', done: false, paso: '2' }
         ];
 
         //Metodo con el cual se agregan nuevos elementos al arreglo
-        todoList.addTodo = function () {
+        kanbanCtrl.addTodo = function () {
             //Añadimos el nuevo elemento al arreglo
-            todoList.kanbanList.push({ titulo: todoList.newTodo, desc: todoList.newTodoDesc, paso: 1 });
+            kanbanCtrl.kanbanList.push({ titulo: kanbanCtrl.newTodo, desc: kanbanCtrl.newTodoDesc, paso: 1 });
 
             //Limpiamos los inputs para en caso de que el usuario desee agregar una tarea nueva
-            todoList.newTodo = "";
-            todoList.newTodoDesc = "";
+            kanbanCtrl.newTodo = "";
+            kanbanCtrl.newTodoDesc = "";
         };
     
         //Metodo que se encarga de mover un elemento hacia adelante o hacia atras
-        todoList.mover = function (todoElement, pasoDestino) {
+        kanbanCtrl.mover = function (todoElement, pasoDestino) {
                 todoElement.paso = pasoDestino;
         }
 
